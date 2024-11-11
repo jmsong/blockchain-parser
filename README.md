13e9425a7eaf2b47498a5fb41049a080dce74838
Tung Tran <minhtunggt@gmail.com>
2024-11-11 12:43:49 +0700

    update: run program using argument instead

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # blockchain-parser
+## You can run the test for the rpc module
+```go
+go test ./rpc -v
+```
+## You can run the test for the parser module
+```go
+go test ./parser -v
+```
+## How to Run the Program
+```go
+go run main.go 0xYourEthereumAddress
+```
--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"blockchain-parser/parser"
 )
 
 func main() {
-	ethParser := parser.NewEthereumParser()
-	ethParser.Subscribe("0xYourEthereumAddress") // Add the Ethereum address to monitor
+	// Check if an Ethereum address was provided as an argument
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide an Ethereum address as an argument")
+	}
 
+	ethAddress := os.Args[1] // Get the Ethereum address from the first argument
+
+	ethParser := parser.NewEthereumParser()
+	if !ethParser.Subscribe(ethAddress) {
+		log.Fatalf("Failed to subscribe to the Ethereum address: %s", ethAddress)
+	}
+
+	log.Printf("Subscribed to Ethereum address: %s", ethAddress)
 	log.Println("Starting the Ethereum block parser...")
 	parser.StartParser(ethParser)
 }
